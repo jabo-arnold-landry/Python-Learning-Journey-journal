b97928052c0e61d567688bda5546636621f3a114
jabo arnold landry <arnoldjab@gmail.com>
2025-07-03 10:26:56 +0200

    date-and-times-in-python

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-#code goes here!
-print('hello Africa')
+# time and date
+import datetime
+
+date = datetime.date(2025, 2, 3) # gets the date bassed on what you inputed
+today = datetime.date.today() # gets todays dates
+time = datetime.time(12, 30,0) # gets the hours
+now = datetime.datetime.now() # get the current time(i.e: time and dates)
+
+print(time)
