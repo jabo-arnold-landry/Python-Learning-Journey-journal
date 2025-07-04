21921e3349af8fdbc975fba31b5281804c3e1f3b
jabo arnold landry <arnoldjab@gmail.com>
2025-07-04 09:34:22 +0200

    threading-with-functions-with-arguments

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import threading # we first import threading modules from python builtin modules
 
 # and to use it we call the thread constructor on it and pass in the function which is will is taking too long to complete and then we call it using the start method to start executing
 import time
-def bathing(name):
+def bathing(fname, lname):# if I had the parameters like this 
     time.sleep(6)
-    print(f"me {name} I am bathing")
+    print(f"me {fname} {lname} I am bathing")
 def brushing():
     time.sleep(2)
     print("I am brushing")
@@ -14,7 +14,7 @@ def breakfast():
     time.sleep(4)
     print("I am having breakfast")
 
-preparation1 = threading.Thread(target = bathing)
+preparation1 = threading.Thread(target = bathing, args=('landry','arnold')) # if we have argument in the function we are trying to pass in as the thread we add the second parameter which is args and assign a tuple of arguments we would want to give to the function
 preparation1.start()
 preparation2 = threading.Thread(target = brushing)
 preparation2.start()
