8734a8875334b0c046ed4ea536ab5933af04f3c2
jabo arnold landry <arnoldjab@gmail.com>
2025-07-05 11:27:25 +0200

    comparing-linear-and-merge-sort-algorithms

--- main.py
+++ main.py
@@ -1,9 +1,40 @@
-# time and date
-import datetime
+import random
 
-date = datetime.date(2025, 2, 3) # gets the date bassed on what you inputed
-today = datetime.date.today() # gets todays dates
-time = datetime.time(12, 30,0) # gets the hours
-now = datetime.datetime.now() # get the current time(i.e: time and dates)
+# Generate a larger dataset with 100 random integers between 1 and 999
+large_dataset = [random.randint(1, 999) for _ in range(100)]
 
-print(time)
+# sorting algorithm
+def sorting(list):
+    if len(list) <= 1:
+        return list
+    mid_of_list = len(list)//2
+    left_hand = sorting(list[mid_of_list:])
+    right_hand = sorting(list[:mid_of_list])
+    return merge_sorted(left_hand, right_hand)
+
+# function to merge the sorted element
+def merge_sorted(coll1, coll2):
+    merged_element = []
+    left_index = 0
+    right_index = 0
+    while left_index < len(coll1) and right_index < len(coll2):
+        if coll1[left_index] < coll2[right_index]:
+           merged_element.append(coll1[left_index]) 
+           left_index+=1
+        else:
+            merged_element.append(coll2[right_index]) 
+            right_index+=1
+    while left_index < len(coll1):
+        merged_element.append(coll1[left_index])
+        left_index +=1
+    while right_index < len(coll2):
+        merged_element.append(coll2[right_index])
+        right_index +=1
+    return merged_element
+
+print(sorting(large_dataset))
+
+
+
+# print("Original dataset:")
+# print(large_dataset)
